88ac43d6085dda0a5cb721354f1d2d0650a0c237
Gilgamesh <devnich@gmail.com>
2022-05-11 14:46:59 -0700

    Expand axes

--- asset_regime.py
+++ asset_regime.py
@@ -25,16 +25,25 @@ df = pd.read_csv(raw_path.joinpath("asset_regime.csv"), index_col="Category")
 fig = plt.figure()
 ax = fig.add_subplot(111, projection = '3d')
 
+ax.set_xlabel("Growth")
+ax.set_ylabel("Inflation")
+ax.set_zlabel("Rates")
+
+ax.set_xlim3d([-4, 4])
+ax.set_ylim3d([-4, 4])
+ax.set_zlim3d([-4, 4])
+
 x = df['Growth']
 y = df['Inflation']
 z = df['Rates']
 
 ax.scatter(x, y, z)
-for xi, yi, zi, label  in zip(x, y, z, df.index):
+
+# Add labels to dots
+for xi, yi, zi, label in zip(x, y, z, df.index):
     ax.text(xi, yi, zi, label)
 
-ax.set_xlabel("Growth")
-ax.set_ylabel("Inflation")
-ax.set_zlabel("Rates")
-
 plt.show()
+
+# This freezes until plt.show() is called the first time
+# fig.show()
